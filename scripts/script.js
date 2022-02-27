2cb85bdfa76a9ad775343feabc23533229f5c2cf
Andrej Bozic <abozic389@gmail.com>
2022-02-27 11:30:08 +0100

    Getting the data from the API and logging it in the console.

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,24 @@ const weatherModule = (() => {
   const form = document.querySelector("#search-city");
   const input = form.querySelector("#city-name");
 
-  async function fetchWeather(e) {
-    const cityName = input.value;
-    const data = await fetch();
+  async function fetchWeather() {
+    try {
+      const cityName = input.value;
+      input.value = "";
+      const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a2b73bbf19c4525e272278eaecbef43d`, { mode: "cors" }).then((response) => response.json());
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   const init = () => {
     form.addEventListener("submit", fetchWeather);
   };
+
+  return {
+    init,
+  };
 })();
+
+weatherModule.init();
